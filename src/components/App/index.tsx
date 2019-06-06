753a5b3f1a97ab07f024354d8ed545942eda0a87
Yu Mao <maoyu960320@hotmail.com>
2019-06-06 10:04:53 +0800

    clean code

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Avatar from '../Avatar';
-import Playground from '../Playground';
 
 import './style.sass';
 
@@ -8,16 +6,7 @@ export default class App extends React.Component<any> {
   render() {
     return (
       <div className="container">
-        <Avatar
-          src="http://p2.music.126.net/6CDzo-lIA0FIu6GwQrXqeQ==/3285340751745782.jpg"
-          shape="circle"
-          size="small"
-        />
-        <Avatar
-          src="http://p2.music.126.net/6CDzo-lIA0FIu6GwQrXqeQ==/3285340751745782.jpg"
-          shape="circle"
-        />
-        <Playground />
+        Talk is cheap, show me the code!
       </div>
     );
   }
